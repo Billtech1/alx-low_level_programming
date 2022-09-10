8c12fac3132b6bdbfe4df1f9baf2a0821a668290
Billtech1 <asogwagodfirst@gmail.com>
2022-09-09 19:17:50 -0700

    zero.variable

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Determine if a numberbus poitive, negetive or zero.
+ *
+ * Return: 0
+ * */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negetive\n", n);
+	else
+		printf("%i is zero\n", n);
+
+	return (0);
+}
