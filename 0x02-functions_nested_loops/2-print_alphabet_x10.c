1ad79cbb16b2530f241b25f8c4fe25b30c2ab47d
Billtech1 <asogwagodfirst@gmail.com>
2022-09-13 19:55:10 -0700

    My second loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,7 @@
  * Return: Always 0
  *
  */
-void print_alphabet_×10(void)
+void print_alphabet(void)
 {
 	int alphabet;
 	int count;
@@ -16,7 +16,7 @@ void print_alphabet_×10(void)
 	{
 		for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
 		{
-			_puthchar(alphabet);
+			_putchar(alphabet);
 		}
 
 		count++;
