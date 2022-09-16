2e633b5e8245d903b541e40815908a22ed957688
Billtech1 <asogwagodfirst@gmail.com>
2022-09-15 19:35:52 -0700

    First loop loop

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit 0 through 9
+ * @c: is a digit to test
+ * Return: Always 0 (success 1)
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
