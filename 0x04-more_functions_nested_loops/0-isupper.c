d502aee9d197e2d0c1785c7518674c5ba7f84dec
Billtech1 <asogwagodfirst@gmail.com>
2022-09-15 19:27:01 -0700

    Zero loop loop

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper -  checks for uppercase characters
+ * _isupper - checks for uppercase characters
  * @c: is the tested character
  * Return: 1 if c is uppercase else 0
  */
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _isupper(int c);
@@ -10,4 +12,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_ numuber(int n)
+void print_numuber(int n);
+
+#endif
