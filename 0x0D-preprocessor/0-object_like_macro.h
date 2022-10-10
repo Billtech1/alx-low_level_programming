a7cb62c9bf360b54eb67d20a3018626c6b28e8e5
Billtech1 <asogwagodfirst@gmail.com>
2022-10-10 15:26:18 -0700

    My zero task

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef _0_object_like_macro_h_
+#define  _0_object_like_macro_h_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+#define SIZE 1024
+
+#endif /*_0_object_like_macro_h_*/
