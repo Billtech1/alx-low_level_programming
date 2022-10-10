e826ffa1e816b046b1c0f8fe788f9c01aa1db049
Billtech1 <asogwagodfirst@gmail.com>
2022-10-10 16:39:05 -0700

    My last preprocessor task

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef _4_sum_h_
+#define _4_sum_h_
+
+/*
+ * Structs, enums and union definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /*_4_sum_h_*/
