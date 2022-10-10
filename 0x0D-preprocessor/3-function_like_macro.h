02c9ea3ff9c07cb7dc7ff098d398353c81bad9e9
Billtech1 <asogwagodfirst@gmail.com>
2022-10-10 16:27:50 -0700

    My 4th task

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef _3_function_like_macro_h_
+#define _3_function_like_macro_h_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+
+#endif /*_3_function_like_macro_h_*/
